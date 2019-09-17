753bcf6918b8a018e32229f8b6230bf08888f31d
Eugen Cazacu <eugen.cazacu@cygni.se>
2019-09-17 16:45:04 +0200

    refactor markup in Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,18 @@
 <template>
   <div class="home">
-    <ToggleThemeButton />
-    <div v-if="error" class="alert alert-danger" role="alert">
-      {{ error }}
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <ToggleThemeButton />
+          <LoginForm />
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <div v-if="error" class="alert alert-danger mt-4" role="alert">{{ error }}</div>
+        </div>
+      </div>
     </div>
-    <LoginForm />
   </div>
 </template>
 
