7a9e68581ad24e9fab184db0b36193e5689ee46f
Eugen Cazacu <eugen.cazacu@cygni.se>
2019-09-17 13:28:04 +0200

    test Msg.vue

---
+++ tests/unit/components/Msg.spec.js
@@ -0,0 +1,27 @@
+import { shallowMount } from "@vue/test-utils";
+import Msg from "@/components/Msg.vue";
+import { isIterable } from "core-js";
+
+describe("Msg.vue", () => {
+  it("renders message correctly", () => {
+    const wrapper = shallowMount(Msg, {
+      propsData: {
+        username: "foo",
+        message: "bar baz boo"
+      }
+    });
+    expect(wrapper.contains("div")).toBe(true);
+    expect(
+      wrapper
+        .find("div")
+        .find("strong")
+        .text()
+    ).toBe("foo :");
+    expect(
+      wrapper
+        .find("div")
+        .find("span")
+        .text()
+    ).toBe("bar baz boo");
+  });
+});
