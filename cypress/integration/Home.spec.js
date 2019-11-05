a0ea0e2ffc5be8f550e6f8fff08f5f6bb2889d13
Eugen Cazacu <eugen.cazacu@cygni.se>
2019-11-05 10:09:38 +0100

    more E2E tests

--- cypress/integration/Home.spec.js
+++ cypress/integration/Home.spec.js
@@ -11,4 +11,19 @@ describe("The Home Page", function() {
     cy.get("#app > .home > .container > .row > .col > span").click();
     cy.get("body.light");
   });
+
+  it("Submit Button is disabled by default", function() {
+    cy.get('button[type="submit"][disabled]')
+    cy.get('input[placeholder="username"]').type("foo");
+    cy.get('input[placeholder="https://github.com/user/repo"]').type("bar");
+    cy.get('button[type="submit"]').click();
+  });
+
+  it("Shows error message when repo is private or not found", function() {
+    cy.get('button[type="submit"][disabled]')
+    cy.get('input[placeholder="username"]').type("foo");
+    cy.get('input[placeholder="https://github.com/user/repo"]').type("bar");
+    cy.get('button[type="submit"]').click();
+    cy.get(".alert-danger");
+  });
 });
